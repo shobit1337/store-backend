3bef09f5267012cc740bd8724a46cde24d6c1002
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-06-24 01:09:38 +0530

    fix: typo

--- models/user.js
+++ models/user.js
@@ -2,42 +2,45 @@ var mongoose = require("mongoose");
 const crypto = require("crypto");
 const uuidv1 = require("uuid/v1");
 
-var userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    require: true,
-    maxlength: 32,
-    trim: true,
+var userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 32,
+      trim: true,
+    },
+    lastname: {
+      type: String,
+      maxlength: 32,
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    userinfo: {
+      type: String,
+      trim: true,
+    },
+    encrypt_password: {
+      type: String,
+      required: true,
+    },
+    salt: String,
+    role: {
+      type: Number,
+      default: 0,
+    },
+    purchases: {
+      type: Array,
+      default: [],
+    },
   },
-  lastname: {
-    type: String,
-    maxlength: 32,
-    trim: true,
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true,
-  },
-  userinfo: {
-    type: String,
-    trim: true,
-  },
-  encrypt_password: {
-    type: String,
-    required: true,
-  },
-  salt: String,
-  role: {
-    type: Number,
-    default: 0,
-  },
-  purchases: {
-    type: Array,
-    default: [],
-  },
-});
+  { timestamps: true }
+);
 
 // Virtual Fields
 userSchema
@@ -45,7 +48,7 @@ userSchema
   .set(function (password) {
     this._password = password; // storing our passowrd in a private variable
     this.salt = uuidv1();
-    this.encrpt_password = this.encrptPassword(password);
+    this.encrypt_password = this.encryptPassword(password);
   })
   .get(function () {
     return this._password; // In case someone need the password
@@ -54,10 +57,10 @@ userSchema
 // Schema Methods - Create as much you want..
 userSchema.methods = {
   authenticate: function (plainPassword) {
-    return this.encrptPassword(plainPassword) === this.encrpt_password;
+    return this.encryptPassword(plainPassword) === this.encrypt_password;
   },
 
-  encrptPassword: function (plainPassword) {
+  encryptPassword: function (plainPassword) {
     if (!plainPassword) return "";
     try {
       return crypto
