4b38bdea8a016c5ef9bf7fe5495b3cf255fdc281
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-07-08 09:56:00 +0530

    feat: validator added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,8 +2,17 @@
 
 // name it same as it was export.. not necessory but its a good practice
 const User = require("../models/user");
+const { validationResult } = require("express-validator");
 
 exports.signup = (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
+
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,24 @@
 var express = require("express");
 var router = express.Router();
-
+const { check } = require("express-validator");
 const { signout, signup } = require("../controllers/auth");
 
-router.post("/signup", signup);
+// we apply express-validator after the routes and before the controllers
+router.post(
+  "/signup",
+  [
+    // you can put as many checks as you like
+    check(
+      "name",
+      "Your Message here!! Name should be atleat 3 character!"
+    ).isLength({ min: 3 }),
+    check("email", "Email is required!").isEmail(),
+    check("password", "Password should be atleast 8 letter long!").isLength({
+      min: 8,
+    }),
+  ],
+  signup
+);
 router.get("/signout", signout);
 
 module.exports = router;
