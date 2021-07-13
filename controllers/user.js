6099bbd1de5a2f4302021162d869a9bffb160905
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-07-13 23:00:11 +0530

    feat: added update user routes

--- controllers/user.js
+++ controllers/user.js
@@ -16,8 +16,26 @@ exports.getUserById = (req, res, next, id) => {
 exports.getUser = (req, res) => {
   // Removing unnecssorry fields from profile So, It cant be accesed by frontend.
   req.profile.salt = undefined;
-  req.profile.encry_password = undefined;
+  req.profile.encrypt_password = undefined;
 
   // Sending json request
   return res.json(req.profile);
+};
+
+exports.updateUser = (req, res) => {
+  User.findByIdAndUpdate(
+    { _id: req.profile._id },
+    { $set: req.body }, // the values we want to update we pass them with $ sign
+    { new: true, useFindAndModify: false },
+    (err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: "You are not authorized to update this user",
+        });
+      }
+      user.salt = undefined;
+      user.encrypt_password = undefined;
+      res.json(user);
+    }
+  );
 };
