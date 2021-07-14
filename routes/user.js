af14e8abb70b2211bf57c160a6084db214b1c226
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-07-14 20:27:28 +0530

    feat: userPurchaseList route added

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUserById, getUser, updateUser } = require("../controllers/user");
+const {
+  getUserById,
+  getUser,
+  updateUser,
+  userPurchaseList,
+} = require("../controllers/user");
 const { isSignedIn, isAuthenticated } = require("../controllers/auth");
 
 router.param("userId", getUserById); // this get called automatically when :userId is used in other requests.
 
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
+router.put(
+  "/orders/user/:userId",
+  isSignedIn,
+  isAuthenticated,
+  userPurchaseList
+);
 
 module.exports = router;
