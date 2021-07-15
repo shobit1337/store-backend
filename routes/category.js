f7055435ba8e03885b6c64793769e62fa99d9c84
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-07-15 08:48:59 +0530

    feat: create category route added

--- routes/category.js
+++ routes/category.js
@@ -1,11 +1,21 @@
 var express = require("express");
 var router = express.Router();
 
-const { getCategoryById } = require("../controllers/category");
+const { getCategoryById, createCategory } = require("../controllers/category");
 const { isAuthenticated, isAdmin, isSignedIn } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
 
+// params
 router.param("userId", getUserById); // this get called automatically when :userId is used in the paramaeter.
 router.param("categoryId", getCategoryById);
+
+//routes
+router.post(
+  "/category/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createCategory
+);
 
 module.exports = router;
