519432a5c6307b69b019874c83252e30d5b2e453
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-07-18 20:32:55 +0530

    feat: added create product route

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,7 @@
 const Product = require("../models/product");
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs"); // This is file system it comes by default
 
 exports.getProductById = (req, res, next, id) => {
   Product.findById(id)
@@ -12,4 +15,41 @@ exports.getProductById = (req, res, next, id) => {
       res.json(product);
       next();
     });
+};
+
+exports.createProduct = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true; // To keep the extensions of our files
+
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Sorry, File failed to upload",
+      });
+    }
+    // TODO: restrictions on field
+    let product = new Product(fields);
+
+    //handle file here
+    if (file.photo) {
+      if (file.photo.size > 3000000) {
+        return res.status(400).json({
+          error: "File size is more then 3mb",
+        });
+      }
+      // TODO: Check if we are uplading image only
+      product.photo.data = fs.readFileSync(file.photo.path);
+      product.photo.contentType = file.photo.type;
+    }
+
+    // Save to db
+    product.save((err, product) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Filed to save in DB",
+        });
+      }
+      res.json(product);
+    });
+  });
 };
--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,20 @@
 var express = require("express");
 var router = express.Router();
 
-const { getProductById } = require("../controllers/product");
+const { getProductById, createProduct } = require("../controllers/product");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
 
 // Params
 router.param("userId", getUserById);
 router.param("productId", getProductById);
+
+router.post(
+  "/product/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createProduct
+);
 
 module.exports = router;
