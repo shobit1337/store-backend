58d64090d55dc2ecaba0d445d067998a0a655c49
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-07-20 18:48:12 +0530

    feat: added getOrderById param

--- controllers/order.js
+++ controllers/order.js
@@ -1,1 +1,16 @@
 const { Order, Cart } = require("../models/order");
+
+// Param controller
+exports.getOrderById = (req, res, next, id) => {
+  Order.find(id)
+    .populate("products.product", "name price") // TODO: confusing, understand better in frontend
+    .exec((err, order) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Sorry! No order found in database",
+        });
+      }
+      res.order = order;
+      next();
+    });
+};
--- routes/order.js
+++ routes/order.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const {} = require("../controllers/order");
+const { getOrderById } = require("../controllers/order");
 
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
 const { updateStock } = require("../controllers/product");
+
+// Prams
+router.param("userId", getUserById);
+router.param("orderId", getOrderById);
+
+// Actual Routes
 
 module.exports = router;
