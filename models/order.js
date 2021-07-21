8c58192987b889514c3f3eb9eaed7b00cda9cb6e
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-07-21 11:40:17 +0530

    feat: Added enum to orderSchema

--- models/order.js
+++ models/order.js
@@ -20,6 +20,11 @@ const orderSchema = new mongoose.Schema(
       type: Number,
     },
     address: String,
+    status: {
+      type: String,
+      default: "",
+      enum: ["Cancelled", "Delivered", "Shipped", "Processing", "Recieved"],
+    },
     updated: Date,
     user: {
       type: ObjectId, // whenever we use ObjectID we also need to provide reference of it
